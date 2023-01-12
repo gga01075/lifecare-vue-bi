a0566479d4965f635f4e645537a820c64d72b232
HSH <gga01075@naver.com>
2023-01-12 17:44:36 +0900

    컴포넌트 고민중...

--- lifecare-vue-chart/src/main.js
+++ lifecare-vue-chart/src/main.js
@@ -3,8 +3,6 @@ import App from './App.vue';
 import router from './router';
 // axios
 import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
 const app = createApp(App);
 
 app.config.globalProperties.bosGrade = '';
