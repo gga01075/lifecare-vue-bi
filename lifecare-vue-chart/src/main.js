49d5e46b96a1f6eb5071036aa1ce4392c6cb33ab
HSH <gga01075@naver.com>
2023-01-12 17:38:34 +0900

    env 및 axios 환경 추가

--- lifecare-vue-chart/src/main.js
+++ lifecare-vue-chart/src/main.js
@@ -1,10 +1,38 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-// rMateChartH5 차트 컴포넌트
-// import Chart from './components/rMateChartH5.vue';
-
+// axios
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+const app = createApp(App);
 
+app.config.globalProperties.bosGrade = '';
+
+axios.defaults.baseURL =
+	process.env.VUE_APP_PROTOCAL + '://' + process.env.VUE_APP_API_HOST;
+axios.defaults.timeout = 990000;
+axios.defaults.credentials = true;
+axios.defaults.withCredentials = true;
+app.config.globalProperties.$axios = axios;
+
+const bosLogin = async () => {
+	console.log('bosLogin: ', 'start');
+	const param = {
+		id: 'biadmin',
+		password: '1234',
+		pin: '',
+		domainType: 'BOS',
+	};
+	// pin :
+	// domainType : 권한(BOS: 관리자, CST: 협력사, SAM : ?) 확인 필요
+
+	axios.post('/mng/signIn', param).then(res => {
+		console.log('res: ', res);
+	});
+};
+console.log('process.env.TYPE: ', process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'local') {
+	bosLogin();
+}
 createApp(App).use(router).mount('#app');
